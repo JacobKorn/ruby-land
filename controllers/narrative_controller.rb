abd45992166057ab3c5ef428a57200b695d52544
Katherine <katherineanderson9@gmail.com>
2014-10-31 14:23:09 +1300

    newer narrative stuff

--- controllers/narrative_controller.rb
+++ controllers/narrative_controller.rb
@@ -1,7 +1,10 @@
 class NarrativeController
+
+	attr_reader :narrative
 	
 	def initialize
 		@narrative = Narrative.new
 	end
+
 
 end	
--- view.rb
+++ view.rb
@@ -1,16 +1,19 @@
 class View
 
 	def initialize
+		puts ""
 		puts "                                        Welcome to                     "
 		puts "                     _____       _             _                     _ "
- 		puts "                    |  __ \\\\     | |           | |                   | |"
+ 		puts "                    |  __ \\     | |           | |                   | |"
 		puts "                    | |__) |   _| |__  _   _  | |     __ _ _ __   __| |"
-		puts "                    |  _  / | | | '_ \\\\| | | | | |    / _` | '_ \\\\ / _` |"
-		puts "                    | | \\\\ \\\\ |_| | |_) | |_| | | |___| (_| | | | | (_| |"
-		puts "                    |_|  \\\\_\\\\__,_|_.__/ \\\\__, | |______\\\\__,_|_| |_|\\\\__,_|"
+		puts "                    |  _  / | | | '_ \\| | | | | |    / _` | '_ \\ / _` |"
+		puts "                    | | \\ \\ |_| | |_) | |_| | | |___| (_| | | | | (_| |"
+		puts "                    |_|  \\_\\__,_|_.__/ \\__, | |______\\__,_|_| |_|\\__,_|"
 		puts "                                        __/ |                          "
 		puts "                                       |___/                           "
-		
+		puts ""
+		puts "To play, pick one of the following locations to complete game and add a gem to your bag!"
+		puts "Once you get 5 gems, trade them in for a ruby and win the game :)"
 		# puts "         ____ /\\ ____            _ _   --           - -__     -_                                         "
 		# puts "        /v y \\/\\/    \\                   --  --___     _ __--__ -" _                                   "
 		# puts "       ____\\7 \\\\_^_^/ \\                            _ --        -_ "-_                                 "
@@ -29,6 +32,15 @@ class View
 		# puts "                        """                                                                               "
 
 	end
+
+	def map_and_locations
+		#
+	end
+
+	def print_gems(gem_array)
+		puts "#print gem array/pics"
+	end
+
 	def volcano
 		puts "You have arrived at a firey volcano! Complete the game to add a gem to your pack!"
 	end
@@ -46,8 +58,13 @@ class View
 	end
 
 	def jungle
-		puts "In the depth of the jungle"
+		puts "In the depth of the jungle you've found a new game! Complete it "
+	end
+
+	def won
+		puts
 	end
 end
 
 test = View.new
+test.volcano
